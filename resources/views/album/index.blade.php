c7d0dedfc4be526537ae3be69e31ae129832ee56
¨Mateus <mateusfigueredo037@gmail.com>
2022-05-31 22:19:20 -0300

    default layout fineshed

--- resources/views/album/index.blade.php
+++ resources/views/album/index.blade.php
@@ -1,13 +1,15 @@
 @extends('default.layout')
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+
+@section('content')
+    <form action="">
+        <div class="row">
+            <div  class="d-flex form-group col-sm-10 col-lg-10">
+                <div  id="search" class="">
+                    <label class="col form-label" for="search">Digite uma palavra chave</label>
+                    <input class="form-control" type="text" name="album" id="">
+                </div>
+                <button id="" class="btn btn-primary" type="submit">Procurar</button>
+            </div>
+        </div>
+    </form>
+@endsection
--- resources/views/default/layout.blade.php
+++ resources/views/default/layout.blade.php
@@ -3,20 +3,27 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Discografia</title>
     <link rel="stylesheet" href="{{asset('web/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{asset('web/style.css')}}">
   </head>
   <body>
-    <h1>Hello Word, Carai</h1>
-
-    <section>
-      <form class="form">
-        <label class="form-label" for="">
-          <input class="form-control" type="text">
-        </label>
-      </form>
-    
+  
+    <section id="main" class="d-flex justify-content-center">
+      <div id="container" class="container">
+        <div id="head" class="d-flex justify-content-between align-items-center">
+          <div class="logo"><img src="{{asset('images/logo.png')}}" alt="logo"></div>
+          <h1>Discografia</h1>
+        </div>
+        <div id="cont" class="container" >
+          @yield('content')
+        
+        </div>
+      </div>
     </section>
+    <footer>
+
+    </footer>
     
   </body>
   <script src="{{asset('web/jquery.js')}}"></script>
